64f0af1828706b26cbc927c5c8b1dd5d38587fe6
@Marcelinha <marcelanalesso2@gmail.com>
2026-05-08 19:14:40 -0300

    pequeno detalhe

---
+++ Jogo_Cleitin/Assets/Scripts/Jogar.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Jogar : MonoBehaviour
+{
+    public void IrParaPrincipal()
+    {
+        SceneManager.LoadScene("Principal"); 
+    }
+}
