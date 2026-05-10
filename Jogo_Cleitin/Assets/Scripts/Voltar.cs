bac43ddecd83152fa7a13ea9de91cfc003361d3f
@Marcelinha <marcelanalesso2@gmail.com>
2026-05-10 19:59:32 -0300

    criando e finalizando as cenas de derrota e vitoria das fases normais

---
+++ Jogo_Cleitin/Assets/Scripts/Voltar.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Voltar : MonoBehaviour
+{
+    public void IrParaPrincipal()
+    {
+        SceneManager.LoadScene("Principal"); 
+    }
+}
