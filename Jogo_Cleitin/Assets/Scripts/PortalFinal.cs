21c905174aa78c585bef8538faf7a9f0da730fed
@Marcelinha <marcelanalesso2@gmail.com>
2026-05-11 16:49:26 -0300

    quase pronto

---
+++ Jogo_Cleitin/Assets/Scripts/PortalFinal.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalFinal : MonoBehaviour
+{
+    public string nomeDaCena;
+    public GameProgress dados;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            int paginasColetadas = ContarPaginas();
+
+            if (paginasColetadas == 3)
+            {
+                SceneManager.LoadScene("Boss");
+            }
+            else
+            {
+                Debug.Log("Faltam páginas!");
+            }
+        }
+    }
+
+    int ContarPaginas()
+    {
+        int total = 0;
+
+        for (int i = 1; i < dados.bossesDerrotados.Length; i++)
+        {
+            if (dados.bossesDerrotados[i] == 1)
+            {
+                total++;
+            }
+        }
+
+        return total;
+    }
+}
